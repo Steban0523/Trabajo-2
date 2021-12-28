2c720e5e2cc316b30117720087953bc5c28de38e
Steban0523 <96693259+Steban0523@users.noreply.github.com>
2021-12-27 20:54:44 -0500

    Update leercadena.c

--- leercadena.c
+++ leercadena.c
@@ -5,51 +5,39 @@
  * Modificado por: John Sanabria - john.sanabria@correounivalle.edu.co
  * Fecha: 2021-02-26
  */
-#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include "leercadena.h"
 
-/**
- * Lee un cadena por teclado.
- * @param[in] size tamano del buffer representado en cadena
- * @param[in] cadena buffer donde se almacenara lo leido de teclado
- * @param[out] tamano de la cadena leida
-*/
-
-int leer_de_teclado(int size, char* cadena) {
-  fgets(cadena, size, stdin);
-  cadena[strlen(cadena) - 1] = '\x0';
-
-  return strlen(cadena);
-}
-
-char** de_cadena_a_vector(char* cadena) {
+int main(int argc, char *argv[]) {
+  char comando[BUFSIZ];
+  char **vector;
   int i;
-  char *token;
-  char *delim = " ";
-  char** resultado;
 
-  resultado = (char**)malloc(sizeof(char*));
-  assert(resultado != NULL);
-  i = 0;
-  token = strtok(cadena,delim);
-  while ( token != NULL) {
-    int cad_longitud;
-    char **result_temp;
-    cad_longitud = strlen(token) + 1;
-    // Copiar token en la posicion 'i'
-    resultado[i] = strdup(token);
-    //printf("|%s|\n",resultado[i]); fflush(stdout);
-    // En busca de la proxima cadena
-    token = strtok(NULL, delim);
-    i++;
-    result_temp = realloc(resultado, (i + 1)  * sizeof(*resultado));
-    assert(result_temp != NULL);
-    resultado = result_temp;
+  while (1) {
+    pid_t pid;
+    printf("Indique el programa que desea se ejecute $ ");
+    leer_de_teclado(BUFSIZ,comando);
+    if (strcmp("exit",comando) == 0) {
+      break;
+    }
+    pid = fork();
+    if (pid < 0) {
+      perror("No se pudo crear un proceso\n");
+      exit(1);
+    } else if (pid == 0) {
+      vector = de_cadena_a_vector(comando);
+      execvp(vector[0],vector);
+      perror("Return from execlp() not expected");
+      exit(EXIT_FAILURE);
+    } else {
+      wait(NULL);
+    }
   }
 
-  resultado[i] = NULL;
-  return resultado;
+  return 0;
 }
